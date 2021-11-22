9cab61e069cb4790ab98c1082ba107a2396d7b48
rpc5255 <41126486+rpc5255@users.noreply.github.com>
2021-11-22 17:39:29 -0500

    added helper functions for toDFA

--- src/NFADFAConv.hs
+++ src/NFADFAConv.hs
@@ -2,7 +2,21 @@ module NFADFAConv where
 
 import NFA 
 
+-- we will incremently convert the NFA to a DFA. The alphabet will never be changed.
+
+changeStates :: NFA -> NFA 
+changeStates = undefined 
+
+changeTransitions :: NFA -> NFA 
+changeTransitions = undefined 
+
+changeStartState :: NFA -> NFA 
+changeStartState = undefined 
+
+changeAcceptStates :: NFA -> NFA 
+changeAcceptStates = undefined 
+
 toDFA ::  NFA -> NFA
-toDFA = undefined 
+toDFA nfa = changeAcceptStates (changeStartState (changeTransitions (changeStates nfa)))
 
 -- there will be many helper functions

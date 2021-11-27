60a90fe78943655357e2c62e1d69131eacbdef66
rpc5255 <41126486+rpc5255@users.noreply.github.com>
2021-11-27 11:42:30 -0500

    nfa to dfa conv

--- src/NFADFAConv.hs
+++ src/NFADFAConv.hs
@@ -1,24 +1,45 @@
 module NFADFAConv where
 
+import Data.Map (Map, (!), (!?))
+import qualified Data.Map as Map
 import Data.Set (Set)
 import qualified Data.Set as Set
 import FA
 
 -- we will incremently convert the NFA to a DFA. The alphabet will never be changed.
 
-changeStates :: Set a -> Set (a, a)
-changeStates = undefined
+changeStates :: NFA a -> Set (Set a)
+changeStates nfa = Set.powerSet (states nfa)
 
-changeTransitions :: NFA a -> NFA a
-changeTransitions = undefined
+changeTransitions :: Ord a => NFA a -> Map (Set a) (Map Char (Set a))
+changeTransitions nfa =
+  let newStates = changeStates nfa
+   in Set.foldr (\x y -> Map.singleton x (createCharMap nfa x (alphabet nfa)) <> y) Map.empty newStates
+  where
+    transitionN' :: Ord a => NFA a -> a -> Char -> Set a
+    transitionN' nfa state char =
+      let charStates = findNeighborsN nfa state (Char char)
+       in flattenMap (transitionEpsilon nfa) charStates
+    newStateTransition :: Ord a => NFA a -> Set a -> Char -> Set a
+    newStateTransition nfa newState char = flattenMap (\s -> transitionN' nfa s char) newState
+    createCharMap :: Ord a => NFA a -> Set a -> Set Char -> Map Char (Set a)
+    createCharMap nfa newState = Set.foldr (\x y -> Map.singleton x (newStateTransition nfa newState x) <> y) Map.empty
 
-changeStartState :: NFA a -> NFA a
-changeStartState = undefined
+changeStartState :: Ord a => NFA a -> Set a
+changeStartState nfa = transitionEpsilon nfa (startState nfa)
 
-changeAcceptStates :: NFA a -> NFA a
-changeAcceptStates = undefined
+changeAcceptStates :: Ord a => NFA a -> Set (Set a)
+changeAcceptStates nfa =
+  let newStates = changeStates nfa
+   in Set.filter (\x -> Set.intersection x (acceptStates nfa) /= Set.empty) newStates
 
-toDFA :: NFA a -> DFA a
-toDFA nfa = undefined
+toDFA :: Ord a => NFA a -> DFA (Set a)
+toDFA nfa =
+  let newStates = changeStates nfa
+      newTransitonMap = changeTransitions nfa
+      newStartState = changeStartState nfa
+      newAcceptStates = changeAcceptStates nfa
+      dfa = F newStates (alphabet nfa) newTransitonMap newStartState newAcceptStates
+   in removeUnreachableStatesD dfa
 
 -- there will be many helper functions

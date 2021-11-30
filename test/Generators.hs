9b49fe7bee18ae4046c06c524fda77cbe42ccbba
rpc5255 <41126486+rpc5255@users.noreply.github.com>
2021-11-30 17:03:26 -0500

    finished generators

--- test/Generators.hs
+++ test/Generators.hs
@@ -14,6 +14,12 @@ Thoughts on how to generate an arbitrary NFA:
 - from that set of states, generate a random set of transitions between the states
 -}
 
+genStates :: forall a. (Ord a, Arbitrary a) => Gen (Set a)
+genStates = Set.fromList <$> listOf1 (arbitrary :: Gen a)
+
+genAlphabet :: Gen (Set Char)
+genAlphabet = Set.fromList <$> listOf1 (arbitrary :: Gen Char)
+
 genSubset :: Set a -> Gen (Set a)
 genSubset set = elements (Set.toList (Set.powerSet set))
 
@@ -22,52 +28,65 @@ genStartState states = do
   i <- chooseInt (0, Set.size states - 1)
   return $ Set.elemAt i states
 
-genSymbolMap :: a -> Set a -> Set Char -> Gen (Map Symbol (Set a))
-genSymbolMap state states alphabet =
-  let symbols = Set.insert Epsilon (Set.map Char alphabet)
-      gens = Set.foldr (\x y -> genSubset states : y) [] symbols
-   in f symbols <$> sequence gens
-  where
-    f :: Set Symbol -> [Set a] -> Map Symbol (Set a)
-    f symbols stateSubset = Map.fromList (zip (Set.toList symbols) stateSubset)
-
-genTransitonMap :: Ord a => Set a -> Set Char -> Gen (Map a (Map Symbol (Set a)))
-genTransitonMap states alphabet =
-  let gens = Set.foldr (\x y -> genSymbolMap x states alphabet : y) [] states
-   in f states <$> sequence gens
-  where
-    f :: Ord a => Set a -> [Map Symbol (Set a)] -> Map a (Map Symbol (Set a))
-    f states maps = Map.fromList (zip (Set.toList states) maps)
-
 instance (Arbitrary a, Ord a) => Arbitrary (NFA a) where
   arbitrary :: Gen (NFA a)
   arbitrary =
-    let genStates = Set.fromList <$> listOf1 (arbitrary :: Gen a)
-        genAlphabet = Set.fromList <$> listOf1 (arbitrary :: Gen Char)
-     in do
-          s <- genStates
-          a <- genAlphabet
-          tm <- genTransitonMap s a
-          ss <- genStartState s
-          as <- genSubset s
-          return (F s a tm ss as)
+    do
+      s <- genStates
+      a <- genAlphabet
+      tm <- genTransitionMapN s a
+      ss <- genStartState s
+      as <- genSubset s
+      return (F s a tm ss as)
+    where
+      genSymbolMap :: a -> Set a -> Set Char -> Gen (Map Symbol (Set a))
+      genSymbolMap state states alphabet =
+        let symbols = Set.insert Epsilon (Set.map Char alphabet)
+            gens = Set.foldr (\x y -> genSubset states : y) [] symbols
+         in f symbols <$> sequence gens
+        where
+          f :: Set Symbol -> [Set a] -> Map Symbol (Set a)
+          f symbols stateSubsets = Map.fromList (zip (Set.toList symbols) stateSubsets)
 
-instance (Arbitrary a, Ord a) => Arbitrary (DFA (Set a)) where
-  arbitrary :: Gen (DFA (Set a))
-  arbitrary = toDFA <$> (arbitrary :: Gen (NFA a))
+      genTransitionMapN :: Set a -> Set Char -> Gen (Map a (Map Symbol (Set a)))
+      genTransitionMapN states alphabet =
+        let gens = Set.foldr (\x y -> genSymbolMap x states alphabet : y) [] states
+         in f states <$> sequence gens
+        where
+          f :: Set a -> [Map Symbol (Set a)] -> Map a (Map Symbol (Set a))
+          f states maps = Map.fromList (zip (Set.toList states) maps)
 
-genNFAString :: NFA a -> Gen String
-genNFAString nfa = listOf (elements (Set.toList (alphabet nfa)))
+instance (Arbitrary a, Ord a) => Arbitrary (DFA a) where
+  arbitrary :: Gen (DFA a)
+  arbitrary =
+    do
+      s <- genStates
+      a <- genAlphabet
+      tm <- genTransitionMapD s a
+      ss <- genStartState s
+      as <- genSubset s
+      return (F s a tm ss as)
+    where
+      genCharMap :: a -> Set a -> Set Char -> Gen (Map Char a)
+      genCharMap state states alphabet =
+        let gens = Set.foldr (\x y -> (arbitrary :: Gen a) : y) [] alphabet
+         in f alphabet <$> sequence gens
+        where
+          f :: Set Char -> [a] -> Map Char a
+          f alphabet randomStates = Map.fromList (zip (Set.toList alphabet) randomStates)
+
+      genTransitionMapD :: Set a -> Set Char -> Gen (Map a (Map Char a))
+      genTransitionMapD states alphabet =
+        let gens = Set.foldr (\x y -> genCharMap x states alphabet : y) [] states
+         in f states <$> sequence gens
+        where
+          f :: Set a -> [Map Char a] -> Map a (Map Char a)
+          f states maps = Map.fromList (zip (Set.toList states) maps)
 
 {-
 -- Generator for strings accepted by this NFA (if any)
 genNFAString :: NFA -> Maybe (Gen String)
 -}
-
-validSymbolMap :: Ord a => Set a -> Set Char -> Map Symbol (Set a) -> Bool
-validSymbolMap states alphabet map =
-  let symbols = Set.insert Epsilon (Set.map Char alphabet)
-   in (Set.fromList (Map.keys map) == symbols) && Map.foldr (\x y -> Set.isSubsetOf x states && y) True map
 
 -- Returns true iff the NFA nfa satisfies all validity properties
 prop_ValidNFA :: Ord a => NFA a -> Bool
@@ -79,7 +98,28 @@ prop_ValidNFA nfa =
       as = acceptStates nfa
    in Set.member ss s && Set.isSubsetOf as s && (Set.fromList (Map.keys tm) == s)
         && Map.foldr (\x y -> validSymbolMap s a x && y) True tm
+  where
+    validSymbolMap :: Ord a => Set a -> Set Char -> Map Symbol (Set a) -> Bool
+    validSymbolMap states alphabet map =
+      let symbols = Set.insert Epsilon (Set.map Char alphabet)
+       in (Set.fromList (Map.keys map) == symbols)
+            && Map.foldr (\x y -> Set.isSubsetOf x states && y) True map
 
 -- Returns true iff the DFA dfa satisfies all validity properties
-prop_ValidDFA :: DFA a -> Bool
-prop_ValidDFA dfa = undefined
+prop_ValidDFA :: Ord a => DFA a -> Bool
+prop_ValidDFA dfa =
+  let s = states dfa
+      a = alphabet dfa
+      tm = transitionMap dfa
+      ss = startState dfa
+      as = acceptStates dfa
+   in Set.member ss s && Set.isSubsetOf as s && (Set.fromList (Map.keys tm) == s)
+        && Map.foldr (\x y -> validCharMap s a x && y) True tm
+  where
+    validCharMap :: Ord a => Set a -> Set Char -> Map Char a -> Bool
+    validCharMap states alphabet map =
+      (Set.fromList (Map.keys map) == alphabet)
+        && Map.foldr (\x y -> Set.member x states && y) True map
+
+genNFAString :: NFA a -> Gen String
+genNFAString nfa = listOf (elements (Set.toList (alphabet nfa)))

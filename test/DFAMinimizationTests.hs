90e0705a797718df3a2617c504a35ea7cbc200fd
rpc5255 <41126486+rpc5255@users.noreply.github.com>
2021-12-04 22:54:19 -0500

    added some more tests

--- test/DFAMinimizationTests.hs
+++ test/DFAMinimizationTests.hs
@@ -12,11 +12,19 @@ import Test.QuickCheck
 -- arbitary strings from the alphabet of d and check if accept d s == accept (minimize d) s
 prop_equivalent :: Ord a => DFA a -> Property
 prop_equivalent dfa = forAll (genString dfa) $
-  \s -> acceptD dfa s == acceptD (minimizeDFA dfa) s
+  \s -> classify (acceptD dfa s) "accepting" $ acceptD dfa s == acceptD (minimizeDFA dfa) s
 
 -- prop_isDFA :: DFA a -> Bool
 prop_isDFA :: Ord a => DFA a -> Bool
-prop_isDFA d = prop_ValidDFA (minimizeDFA d)
+prop_isDFA dfa = prop_ValidDFA (minimizeDFA dfa)
 
-prop_isSmaller :: DFA a -> Bool
-prop_isSmaller d = Set.size (states (minimizeDFA d)) <= Set.size (states d)
+prop_isSmaller :: Ord a => DFA a -> Bool
+prop_isSmaller dfa =
+  let minDFA = minimizeDFA dfa
+   in Set.size (states minDFA) <= Set.size (states dfa)
+
+prop_isMinimal :: Ord a => DFA a -> Bool
+prop_isMinimal dfa =
+  let minDFA = minimizeDFA dfa
+      minDFA' = minimizeDFA minDFA
+   in Set.size (states minDFA) == Set.size (states minDFA')

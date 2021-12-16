81f0dc382649760614a2117ae8e6b99c75490021
Sean Banko <sean.banko@gmail.com>
2021-12-16 14:25:28 -0500

    Cleanup and refactor GNFA functions

--- src/NFAOperations.hs
+++ src/NFAOperations.hs
@@ -10,34 +10,22 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Test.HUnit
 
+-- Renames the states of an NFA, incrementing them by k
 shiftStatesNFA :: Int -> NFA Int -> NFA Int
 shiftStatesNFA k (F s a tm ss as) =
     let shift = (+ k)
-        s' = Set.map shift s
-        a' = a
-        tm' = Map.mapKeys shift (Map.map (Map.map (Set.map shift)) tm)
-        ss' = shift ss
-        as' = Set.map shift as in
-    F s' a' tm' ss' as'
-
--- test_shiftStatesNFA :: Test
--- test_shiftStatesNFA =
---   "shift states tests"
---     ~: TestList
---       [ states (shiftStatesNFA 5 n1) ~?= Set.fromList [6, 7, 8, 9],
---         transitionMap (shiftStatesNFA 5 n1) ! 6 ~?= Map.fromList [(FA.Char '0', Set.singleton 6), (FA.Char '1', Set.fromList [6, 7]), (Epsilon, Set.empty)], 
---         transitionMap (shiftStatesNFA 5 n1) ! 7 ~?= Map.fromList [(FA.Char '0', Set.singleton 8), (FA.Char '1', Set.empty), (Epsilon, Set.singleton 8)], 
---         transitionMap (shiftStatesNFA 5 n1) ! 8 ~?= Map.fromList [(FA.Char '0', Set.empty), (FA.Char '1', Set.singleton 9), (Epsilon, Set.empty)],
---         transitionMap (shiftStatesNFA 5 n1) ! 9 ~?= Map.fromList [(FA.Char '0', Set.singleton 9), (FA.Char '1', Set.singleton 9), (Epsilon, Set.empty)],
---         startState (shiftStatesNFA 5 n1) ~?= 6,
---         acceptStates (shiftStatesNFA 5 n1) ~?= Set.singleton 9
---       ]
+        s'    = Set.map shift s
+        a'    = a
+        tm'   = Map.mapKeys shift (Map.map (Map.map (Set.map shift)) tm)
+        ss'   = shift ss
+        as'   = Set.map shift as
+     in F s' a' tm' ss' as'
 
 -- Creates a transition map for an NFA that, for each state, maps each symbol to the empty set 
 emptyTransitionMapNFA :: Ord a => Set a -> Set Char -> Map a (Map Symbol (Set a))
 emptyTransitionMapNFA states alphabet =
+  let symbols = Set.insert Epsilon (Set.map FA.Char alphabet) in
     Map.fromList (zip (Set.toList states) (repeat (Map.fromList (zip (Set.toList symbols) (repeat Set.empty)))))
-    where symbols = Set.insert Epsilon (Set.map FA.Char alphabet)
 
 -- Creates a copy of the transition map that contains keys and values for each state and symbol
 makeTotalTransitionMapNFA :: Ord a => Set a -> Set Char -> Map a (Map Symbol (Set a)) -> Map a (Map Symbol (Set a))
@@ -46,22 +34,22 @@ makeTotalTransitionMapNFA qs sigma delta = Map.unionWith Map.union delta (emptyT
 -- Creates a transition map for the union of two NFAs, given the new states, alphabet, and start state
 unionTransitionMaps :: Ord a => NFA a -> NFA a -> Set a -> Set Char -> a -> Map a (Map Symbol (Set a))
 unionTransitionMaps n1 n2 qs sigma q0 =
-  let deltaUnion = Map.union (transitionMap n1) (transitionMap n2)
-      q0Map = Map.singleton Epsilon (Set.fromList [startState n1, startState n2])
+  let deltaUnion  = Map.union (transitionMap n1) (transitionMap n2)
+      q0Map       = Map.singleton Epsilon (Set.fromList [startState n1, startState n2])
       deltaWithq0 = Map.insert q0 q0Map deltaUnion
-      delta = makeTotalTransitionMapNFA qs sigma deltaWithq0
+      delta       = makeTotalTransitionMapNFA qs sigma deltaWithq0
    in delta
 
 -- Creates the union of two NFAs
 union :: NFA Int -> NFA Int -> NFA Int
 union n1 n2 = 
-    let n1' = if Set.findMin (states n1) == 0 then shiftStatesNFA 1 n1 else n1 
-        n2' = shiftStatesNFA (Set.findMax (states n1') - Set.findMin (states n2) + 1) n2 
-        qs = Set.unions [Set.singleton q0, states n1', states n2']
+    let n1'   = if Set.findMin (states n1) == 0 then shiftStatesNFA 1 n1 else n1 
+        n2'   = shiftStatesNFA (Set.findMax (states n1') - Set.findMin (states n2) + 1) n2 
+        qs    = Set.unions [Set.singleton q0, states n1', states n2']
         sigma = Set.union (alphabet n1') (alphabet n2')
         delta = unionTransitionMaps n1' n2' qs sigma q0 
-        q0 = 0 
-        fs = Set.union (acceptStates n1') (acceptStates n2')
+        q0    = 0 
+        fs    = Set.union (acceptStates n1') (acceptStates n2')
      in F qs sigma delta q0 fs
 
 -- Adds epsilon transitions from *srcs* to *tgt* in the map
@@ -71,38 +59,38 @@ addEpsilonTransitions srcs tgt delta = foldr (Map.adjust (Map.insertWith Set.uni
 -- Creates a transition map for the concatenation of two NFAs, given the new states and alphabet
 concatenateTransitionMaps :: Ord a => NFA a -> NFA a -> Set a -> Set Char -> Map a (Map Symbol (Set a))
 concatenateTransitionMaps n1 n2 qs sigma = 
-  let deltaUnion = Map.union (transitionMap n1) (transitionMap n2)
+  let deltaUnion        = Map.union (transitionMap n1) (transitionMap n2)
       deltaWithEpsilons = addEpsilonTransitions (acceptStates n1) (startState n2) deltaUnion
-      delta = makeTotalTransitionMapNFA qs sigma deltaWithEpsilons
+      delta             = makeTotalTransitionMapNFA qs sigma deltaWithEpsilons
    in delta
 
 -- Concatenates two NFAs
 concatenate :: NFA Int -> NFA Int -> NFA Int
 concatenate n1 n2 = 
-  let n2' = shiftStatesNFA (Set.findMax (states n1) - Set.findMin (states n2) + 1) n2 
-      qs = Set.union (states n1) (states n2')
+  let n2'   = shiftStatesNFA (Set.findMax (states n1) - Set.findMin (states n2) + 1) n2 
+      qs    = Set.union (states n1) (states n2')
       sigma = Set.union (alphabet n1) (alphabet n2')
       delta = concatenateTransitionMaps n1 n2' qs sigma
-      q0 = startState n1
-      fs = acceptStates n2' 
+      q0    = startState n1
+      fs    = acceptStates n2' 
    in F qs sigma delta q0 fs
 
 -- Creates a transition map for the star of an NFA, given the new states, alphabet, and start state
 starTransitionMap :: Ord a => NFA a -> Set a -> Set Char -> a -> Map a (Map Symbol (Set a))
 starTransitionMap n qs sigma q0 = 
-  let q0Map = Map.singleton Epsilon (Set.singleton (startState n))
-      deltaWithq0 = Map.insert q0 q0Map (transitionMap n)
+  let q0Map             = Map.singleton Epsilon (Set.singleton (startState n))
+      deltaWithq0       = Map.insert q0 q0Map (transitionMap n)
       deltaWithEpsilons = addEpsilonTransitions (acceptStates n) (startState n) deltaWithq0
-      delta = makeTotalTransitionMapNFA qs sigma deltaWithEpsilons
+      delta             = makeTotalTransitionMapNFA qs sigma deltaWithEpsilons
    in delta
 
 -- Creates the star NFA
 star :: NFA Int -> NFA Int
 star n =
-    let n' = if Set.findMin (states n) == 0 then shiftStatesNFA 1 n else n
-        qs = Set.insert q0 (states n')
+    let n'    = if Set.findMin (states n) == 0 then shiftStatesNFA 1 n else n
+        qs    = Set.insert q0 (states n')
         sigma = alphabet n
         delta = starTransitionMap n' qs sigma q0
-        q0 = 0
-        fs = Set.insert q0 (acceptStates n')
+        q0    = 0
+        fs    = Set.insert q0 (acceptStates n')
      in F qs sigma delta q0 fs
--- src/NFARegexConv.hs
+++ src/NFARegexConv.hs
@@ -54,22 +54,34 @@ toNFA (Star r)          = NFAOperations.star (toNFA r)
 
 type GNFA a = FA a (Map a RegExp)
 
--- creates a transition map for a GNFA mapping each state to each other state with a Void arrow
+-- Creates a transition map for a GNFA that, for each state, maps each state to Void
 voidTransitionMapGNFA :: Ord a => Set a -> a -> a -> Map a (Map a RegExp)
 voidTransitionMapGNFA qs q0 qf = 
-    Map.fromList (zip (Set.toList (Set.delete qf qs)) (repeat (Map.fromList (zip (Set.toList (Set.delete q0 qs)) (repeat RegExp.Void)))))
+    let qis = Set.toList (Set.delete qf qs)
+        qjs = Set.toList (Set.delete q0 qs)
+    in Map.fromList (zip qis (repeat (Map.fromList (zip qjs (repeat RegExp.Void)))))
 
-convertTransitions :: Ord a => DFA a -> a -> a -> Map a (Map a RegExp) 
-convertTransitions d q0 qf = 
-              -- inverts the keys and values of the dfa's transition map
-    let tm    = Map.map (Map.fromListWith alt . map (\(c, q) -> (q, char c)) . Map.toList) (transitionMap d)
-              -- adds an epsilon (empty RegExp) transition from the new start state to the original start state
-        tm'   = Map.insert q0 (Map.singleton (startState d) RegExp.Empty) tm
-              -- adds epsilon (empty RegExp) transitions from the original accept states to the new accept state
-        tm''  = foldr (Map.adjust (Map.insert qf RegExp.Empty)) tm' (acceptStates d)
-              -- adds Void transitions between any states that do not have an arrow connecting them
-        tm''' = Map.unionWith Map.union tm'' (voidTransitionMapGNFA (Set.unions [states d, Set.singleton q0, Set.singleton qf]) q0 qf)
-    in tm'''
+-- Creates a copy of the transition map that contains keys and values for each pair of states
+makeTotalTransitionMapGNFA :: Ord a => Set a -> a -> a -> Map a (Map a RegExp) -> Map a (Map a RegExp)
+makeTotalTransitionMapGNFA qs q0 qf delta = Map.unionWith Map.union delta (voidTransitionMapGNFA qs q0 qf)
+
+-- Inverts the transition map of a DFA for a GNFA
+invert :: Ord a => Map a (Map Char a) -> Map a (Map a RegExp)
+invert = Map.map (Map.fromListWith alt . map (\(c, q) -> (q, char c)) . Map.toList)
+
+-- Adds epsilon transitions from *srcs* to *tgt* for a GNFA
+addEpsilonTransitionsGNFA :: Ord a => Set a -> a -> Map a (Map a RegExp) -> Map a (Map a RegExp)
+addEpsilonTransitionsGNFA srcs tgt delta = foldr (Map.adjust (Map.insert tgt RegExp.Empty)) delta srcs
+
+-- Creates a transition map for the Generalized NFA of a DFA, given the new start state and final state
+generalizeTransitionMap :: Ord a => DFA a -> a -> a -> Map a (Map a RegExp) 
+generalizeTransitionMap d q0 qf = 
+    let tm    = invert (transitionMap d)
+        q0Map = Map.singleton (startState d) RegExp.Empty
+        tm'   = Map.insert q0 q0Map tm
+        tm''  = addEpsilonTransitionsGNFA (acceptStates d) qf tm'
+        tm''' = makeTotalTransitionMapGNFA (Set.unions [states d, Set.singleton q0, Set.singleton qf]) q0 qf tm''
+     in tm'''
 
 toGNFA :: DFA Int -> GNFA Int
 toGNFA d@(F qs sigma tm q0 fs) = 
@@ -78,10 +90,10 @@ toGNFA d@(F qs sigma tm q0 fs) =
         qf' = q0' + 1
         fs' = Set.singleton qf'
         qs' = Set.unions [qs, Set.singleton q0', fs']
-        tm' = convertTransitions d q0' qf'
+        tm' = generalizeTransitionMap d q0' qf'
      in F qs' sigma' tm' q0' fs'
 
--- TODO make this safe
+-- TODO make this safe?
 convert :: Ord a => GNFA a -> RegExp
 convert g
     | Set.size (states g) == 2  = transitionMap g ! startState g ! Set.elemAt 0 (acceptStates g)
@@ -91,7 +103,7 @@ convert g
 rip :: Eq a => Ord a => GNFA a -> GNFA a
 rip g = case List.find (\q -> (q /= startState g) && notElem q (acceptStates g)) (Map.keys (transitionMap g)) of
     Just qrip -> g {states = Set.delete qrip (states g), transitionMap = ripTransitions g qrip}
-    _ -> error "problem"
+    _ -> error "No valid states found to rip"
 
 ripTransitions :: Ord a => GNFA a -> a -> Map a (Map a RegExp) 
 ripTransitions g qrip =
@@ -99,8 +111,8 @@ ripTransitions g qrip =
         qis   = Set.delete (Set.elemAt 0 (acceptStates g)) q'
         qjs   = Set.delete (startState g) q'
         pairs = Set.toList (Set.cartesianProduct qis qjs)
-        tm = foldr (\(qi, qj) mp -> Map.adjust (Map.insert qj (ripRegExp g qi qj qrip)) qi mp) (transitionMap g) pairs
-        tm' = Map.delete qrip (Map.map (Map.delete qrip) tm)
+        tm    = foldr (\(qi, qj) mp -> Map.adjust (Map.insert qj (ripRegExp g qi qj qrip)) qi mp) (transitionMap g) pairs
+        tm'   = Map.delete qrip (Map.map (Map.delete qrip) tm)
      in tm'
 
 ripRegExp :: Ord a => GNFA a -> a -> a -> a -> RegExp
